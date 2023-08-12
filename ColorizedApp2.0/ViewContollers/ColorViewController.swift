7545f30704e1d90bcf9203b0fa6b810a52f5957f
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-12 16:55:44 +0300

    delete selectedColor

--- ColorizedApp2.0/ViewContollers/ColorViewController.swift
+++ ColorizedApp2.0/ViewContollers/ColorViewController.swift
@@ -15,17 +15,11 @@ class ColorViewController: UIViewController {
     
     // MARK: - Public properties
     var sliderValues = (red: Float(191), green: Float(127), blue: Float(63))
-    var selectedColor = UIColor(
-        red: CGFloat(191) / 255,
-        green: CGFloat(127) / 255,
-        blue: CGFloat(63) / 255,
-        alpha: 1
-    )
-    
+        
     // MARK: - Overridee methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = selectedColor
+        updateViewColor(with: sliderValues)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -35,6 +29,20 @@ class ColorViewController: UIViewController {
         settingsVC.sliderValues = sliderValues
         settingsVC.delegate = self
     }
+    
+    // MARK: - {rivate func
+    private func updateViewColor(with values: (red: Float, green: Float, blue: Float)) {
+        let red = CGFloat(values.red)
+        let green = CGFloat(values.green)
+        let blue = CGFloat(values.blue)
+        
+        view.backgroundColor = UIColor(
+            red: red / 255,
+            green: green / 255,
+            blue: blue / 255,
+            alpha: 1
+        )
+    }
 }
 
 // MARK: - SettingsViewControllerDelegate
@@ -42,14 +50,6 @@ extension ColorViewController: SettingsViewControllerDelegate {
     func setNewColor(with values: (red: Float, green: Float, blue: Float)) {
         sliderValues = values
         
-        let red = CGFloat(values.red)
-        let green = CGFloat(values.green)
-        let blue = CGFloat(values.blue)
-        view.backgroundColor = UIColor(
-            red: red / 255,
-            green: green / 255,
-            blue: blue / 255,
-            alpha: 1
-        )
+        updateViewColor(with: values)
     }
 }
